b9c2b638f29a8d1faedb948161a3b4237156a718
Vipul Nair <vipulnair@PINEMAC0034.local>
2021-07-25 10:05:01 +0530

    dummy website

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const express = require('express');
-const app = express();
+
 console.log("started")
 
-app.use(express.static('public'));
-app.post('/api', (request, response) => {
-
-    console.log(request);
-})
